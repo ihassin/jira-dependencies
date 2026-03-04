5590464d4a06d5a27b9c792b02bd24b1b8c7d491
Itamar <ihassin@gmail.com>
2026-03-04 18:03:27 +0200

    Added png

--- main.rb
+++ main.rb
@@ -14,10 +14,9 @@ graph.add_dependency(ticket: 120, is_blocking: 6)
 graph.add_dependency(ticket: 121, is_blocking: 6)
 graph.add_dependency(ticket: 122, is_blocking: 6)
 graph.add_dependency(ticket: 6, is_blocking: 300)
-graph.add_dependency(ticket: 1020, is_blocking: 300)
+# graph.add_dependency(ticket: 1020, is_blocking: 300)
 
 graph.save_graph(file_name: 'graph.yml')
-
-graph = DependencyGraph.new(file_name: 'graph.yml')
+# graph = DependencyGraph.new(file_name: 'graph.yml')
 
 graph.generate_graph(file_name: 'ingestion.png')
